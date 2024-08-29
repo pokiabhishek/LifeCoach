e90abe0cf64197241ae5ddde84898b0efd6c7e9a
Your Name <your.email@example.com>
2024-08-30 03:35:19 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/LifeCoach/",
   assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf'], 
   server: {
     host: '0.0.0.0', // This allows access from external devices
     port: 3000,      // or any port you are using
   },
-  base:"/LifeCoach/"
 })
